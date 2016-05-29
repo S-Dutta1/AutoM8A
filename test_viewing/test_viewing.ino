224c2bb8b795cc86723bcabf42807ce4d7f44b57
arkyaC <arkya.chat@gmail.com>
2016-05-29 23:11:10 +0530

    viewing angle tested

---
+++ test_viewing/test_viewing.ino
@@ -0,0 +1,19 @@
+int sensor1=A0;
+int sensor2=A1;
+int led=13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+  digitalWrite(led,HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  Serial.print(analogRead(sensor1));
+  Serial.print("        ");
+  Serial.println(analogRead(sensor2));
+  delay(200);
+  
+}
